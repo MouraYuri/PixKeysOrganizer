5f345cfc4d3b985145f0aa764d25ad0bb10d8269
Yuri-Moura <yuri.moura@trilogo.com.br>
2021-02-14 00:07:07 -0300

    Initial Commit

---
+++ PixOrganizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PixOrganizer
+//
+//  Created by Trilogo Tecnologia on 14/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
